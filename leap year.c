96d3f9ac894c11b61b71402ebc654664e25fc737
Shreya1600 <dg5668160@gmail.com>
2020-10-01 20:10:52 +0530

    first commit

---
+++ leap year.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+
+{
+    int year;
+    printf("Enter a year:");
+    scanf("%d",&year);
+    if(year%100!=0)
+    {
+        if(year%4==0)
+            printf("Leap year\n");
+        else
+            printf("Not leap year\n");
+    }
+    else
+    {
+        if(year%400==0)
+            printf("Leap year\n");
+        else
+            printf("Not year\n");
+    }
+  return 0;
+}

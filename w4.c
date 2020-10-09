484cb9545d39b4fe5c849a38eab061f6043d35e7
Shreya1600 <71880316+Shreya1600@users.noreply.github.com>
2020-10-09 13:42:15 +0530

    Add files via upload

---
+++ w4.c
@@ -0,0 +1,13 @@
+//Program to print numbers in reverse order from 1 to 10 using while loop
+#include<stdio.h>
+int main(void)
+{
+    int i=10;
+    while(i>=1)
+    {
+        printf("%d\n",i);
+        i=i-1;
+    }
+    printf("\n");
+    return 0;
+}

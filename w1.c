40a5b3b5e5990754c846dcf15327581a0aed95b2
Shreya1600 <71880316+Shreya1600@users.noreply.github.com>
2020-10-09 14:33:12 +0530

    update w1.c

---
+++ w1.c
@@ -0,0 +1,13 @@
+//Program to print numbers from 1 to 10 using while loop
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i);
+        i=i+1;//statement that changes the value of condition
+    }
+    printf("\n");
+    return 0;
+}

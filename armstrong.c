31ed4b1a6e4157db893b9f6d9dc9c07c667587d9
Shreya1600 <71880316+Shreya1600@users.noreply.github.com>
2020-10-12 19:25:24 +0530

    check armstrong number

---
+++ armstrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+{
+    int n,r,c,sum=0,temp;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        c=r*r*r;
+        sum=sum+c;
+        n/=10;
+    }
+    n=temp;
+    if(n==sum)
+        printf("Armstrong");
+    else
+        printf("Not an Armstrong");
+    return 0;
+}

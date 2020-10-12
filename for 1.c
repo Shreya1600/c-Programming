840efaf5853c5a1135e3f0e96b8b8e2605e25426
Shreya1600 <71880316+Shreya1600@users.noreply.github.com>
2020-10-12 12:55:08 +0530

    print numbers

---
+++ for 1.c
@@ -0,0 +1,11 @@
+//Program to print numbers from 1 to 10 using for loop
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    for(i=1;i<=10;i++)
+       printf("%d\n",i);
+    printf("\n");
+    printf("\nProgram created by Shreya");
+    return 0;
+}

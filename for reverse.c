740cad99cbb82f649f8d66a820e35d63862ccfd7
Shreya1600 <71880316+Shreya1600@users.noreply.github.com>
2020-10-12 12:52:41 +0530

    reverse number

---
+++ for reverse.c
@@ -0,0 +1,11 @@
+//Program to print numbers in reverse order with a difference of two
+#include<stdio.h>
+int main(void)
+{
+    int i;
+    for(i=20;i>=2;i-=2)
+        printf("%d\n",i);
+    printf("\n");
+    return 0;
+}
+
